2e10bf80000fe216d79c666611aad4bd2671186b
naveensagar <sagarnaveen1995@gmail.com>
2019-05-31 15:44:35 +0530

    add project model

---
+++ src/db/models/project.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+// Projects have a name, description, duration, and avatar (image) and can be viewed/edited
+//  from their own uniquely addressed details page.
+
+const Project = mongoose.model('Project', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    startDate: {
+        type: Date,
+        default: Date.now
+    },
+    endDate: {
+        type: Date,
+        default: Date.now
+    }
+
+    // Add date
+})
+
+module.exports = Project
--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 const express = require('express')
+require('./db/mongoose')
+const Project = require('./db/models/project')
 const app = express()
 
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+// ====== ROUTES ==========
+
+app.get('/', (req, res) => {
+    res.send('will redirect to projects page')
+})
+
+// SHOW ROUTE
+app.get('/projects', (req, res) => {
+    res.send('will render projects page')
+})
+
+// CREATE ROUTE
+app.post('/projects', async (req, res) => {
+    const project = new Project(req.body)
+    try {
+        await project.save()
+        res.status(201).send(project)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
