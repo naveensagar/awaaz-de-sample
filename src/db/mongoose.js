abb1a7ea8d32291a0e6496734d46b6d48ee17d04
naveensagar <sagarnaveen1995@gmail.com>
2019-06-02 23:24:47 +0530

    add database url for mongo-atlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 // Connect to mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/awaaz-de-sample-app', {
+const databaseURL = process.env.DATABASEURL || 'mongodb://127.0.0.1:27017/awaaz-de-sample-app'
+mongoose.connect(databaseURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
